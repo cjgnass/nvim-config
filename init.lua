8e950a285af52ba52ec5f02054a1114bc2ba48a0
cjgnass <cjgnass@gmail.com>
2026-02-01 21:27:52 -0500

    added ufo

--- init.lua
+++ init.lua
@@ -3,230 +3,245 @@ require("keymaps")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	"NMAC427/guess-indent.nvim",
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-	{ "tiagovla/tokyodark.nvim", name = "tokyodark", priority = 1000 },
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-		dependencies = { "williamboman/mason.nvim" },
-		config = function()
-			require("mason-tool-installer").setup({})
-		end,
-	},
-	{
-		"stevearc/conform.nvim",
-		config = function()
-			require("conform").setup({
-				formatters_by_ft = {
-					lua = { "stylua" },
-					cpp = { "clang-format" },
-					javascript = { { "prettierd", "prettier" } },
-					typescript = { { "prettierd", "prettier" } },
-					typescriptreact = { { "prettierd", "prettier" } },
-					go = { "gofmt" },
-				},
-				format_on_save = {
-					timeout_ms = 500,
-					lsp_fallback = true,
-				},
-			})
-		end,
-	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		lazy = false,
-		build = ":TSUpdate",
-		config = function()
-			require("nvim-treesitter").setup({
-				highlight = { enable = true },
-				indent = { enable = true },
-			})
-			require("nvim-treesitter").install({
-				"lua",
-				"vim",
-				"query",
-				"c",
-				"cpp",
-				"typescript",
-				"javascript",
-				"tsx",
-				"jsx",
-				"python",
-				"rust",
-				"go",
-				"java",
-			})
-		end,
-	},
-	{
-		"christoomey/vim-tmux-navigator",
-		cmd = {
-			"TmuxNavigateLeft",
-			"TmuxNavigateDown",
-			"TmuxNavigateUp",
-			"TmuxNavigateRight",
-			"TmuxNavigatePrevious",
-			"TmuxNavigatorProcessList",
-		},
-	},
-	{
-		"saghen/blink.cmp",
-		dependencies = { "rafamadriz/friendly-snippets" },
-		version = "1.*",
-		opts = {
-			keymap = { preset = "default" },
-			appearance = {
-				nerd_font_variant = "mono",
-			},
-			completion = { documentation = { auto_show = false } },
-			sources = {
-				default = { "lsp", "path", "snippets", "buffer" },
-			},
-			fuzzy = { implementation = "lua" },
-		},
-		opts_extend = { "sources.default" },
-	},
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			"saghen/blink.cmp",
-			"williamboman/mason-lspconfig.nvim",
-		},
-		config = function()
-			local capabilities = require("blink.cmp").get_lsp_capabilities()
-			local lspconfig = require("lspconfig")
-			require("mason-lspconfig").setup({
-				handlers = {
-					function(server_name)
-						lspconfig[server_name].setup({
-							capabilities = capabilities,
-						})
-					end,
-				},
-			})
-		end,
-	},
-	{ "nvim-tree/nvim-web-devicons", opts = {} },
-	{
-		"ibhagwan/fzf-lua",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		cmd = "FzfLua",
-		opts = {
-			-- Use the 'ivy' layout for a clean bottom-pane feel
-			winopts = {
-				height = 0.40,
-				width = 1.00,
-				row = 1.00,
-				preview = {
-					layout = "flex",
-					flip_threshold = 120,
-					scrollbar = "float",
-				},
-			},
-			keymap = {
-				-- These make the fzf window behave more like your terminal
-				fzf = {
-					["ctrl-q"] = "select-all+accept",
-				},
-			},
-		},
-	},
-	{
-		"akinsho/bufferline.nvim",
-		version = "*",
-		dependencies = "nvim-tree/nvim-web-devicons",
-		config = function()
-			require("bufferline").setup({})
-		end,
-	},
-	{
-		"christoomey/vim-tmux-navigator",
-		cmd = {
-			"TmuxNavigateLeft",
-			"TmuxNavigateDown",
-			"TmuxNavigateUp",
-			"TmuxNavigateRight",
-			"TmuxNavigatePrevious",
-			"TmuxNavigatorProcessList",
-		},
-		keys = {
-			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-		},
-	},
-	{ -- Useful plugin to show you pending keybinds.
-		"folke/which-key.nvim",
-		event = "VimEnter", -- Sets the loading event to 'VimEnter'
-		opts = {
-			-- delay between pressing a key and opening which-key (milliseconds)
-			-- this setting is independent of vim.o.timeoutlen
-			delay = 0,
-			icons = {
-				-- set icon mappings to true if you have a Nerd Font
-				mappings = vim.g.have_nerd_font,
-				-- If you are using a Nerd Font: set icons.keys to an empty table which will use the
-				-- default which-key.nvim defined Nerd Font icons, otherwise define a string table
-				keys = vim.g.have_nerd_font and {} or {
-					Up = "<Up> ",
-					Down = "<Down> ",
-					Left = "<Left> ",
-					Right = "<Right> ",
-					C = "<C-…> ",
-					M = "<M-…> ",
-					D = "<D-…> ",
-					S = "<S-…> ",
-					CR = "<CR> ",
-					Esc = "<Esc> ",
-					ScrollWheelDown = "<ScrollWheelDown> ",
-					ScrollWheelUp = "<ScrollWheelUp> ",
-					NL = "<NL> ",
-					BS = "<BS> ",
-					Space = "<Space> ",
-					Tab = "<Tab> ",
-					F1 = "<F1>",
-					F2 = "<F2>",
-					F3 = "<F3>",
-					F4 = "<F4>",
-					F5 = "<F5>",
-					F6 = "<F6>",
-					F7 = "<F7>",
-					F8 = "<F8>",
-					F9 = "<F9>",
-					F10 = "<F10>",
-					F11 = "<F11>",
-					F12 = "<F12>",
-				},
-			},
+    "NMAC427/guess-indent.nvim",
+    { "catppuccin/nvim",         name = "catppuccin", priority = 1000 },
+    { "tiagovla/tokyodark.nvim", name = "tokyodark",  priority = 1000 },
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        dependencies = { "williamboman/mason.nvim" },
+        config = function()
+            require("mason-tool-installer").setup({})
+        end,
+    },
+    {
+        "stevearc/conform.nvim",
+        config = function()
+            require("conform").setup({
+                formatters_by_ft = {
+                    lua = { "stylua" },
+                    cpp = { "clang-format" },
+                    javascript = { { "prettierd", "prettier" } },
+                    typescript = { { "prettierd", "prettier" } },
+                    typescriptreact = { { "prettierd", "prettier" } },
+                    go = { "gofmt" },
+                },
+                format_on_save = {
+                    timeout_ms = 500,
+                    lsp_fallback = true,
+                },
+            })
+        end,
+    },
+    -- {
+    --     "nvim-treesitter/nvim-treesitter",
+    --     lazy = false,
+    --     build = ":TSUpdate",
+    --     config = function()
+    --         require("nvim-treesitter.configs").setup({
+    --             ensure_installed = {
+    --                 "lua",
+    --                 "vim",
+    --                 "query",
+    --                 "c",
+    --                 "cpp",
+    --                 "typescript",
+    --                 "javascript",
+    --                 "tsx",
+    --                 "jsx",
+    --                 "python",
+    --                 "rust",
+    --                 "go",
+    --                 "java",
+    --             },
+    --             highlight = { enable = true },
+    --             indent = { enable = true },
+    --         })
+    --     end,
+    -- },
 
-			-- Document existing key chains
-			spec = {
-				{ "<leader>s", group = "[S]earch" },
-				{ "<leader>t", group = "[T]oggle" },
-				{ "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
-			},
-		},
-	},
+    {
+        "saghen/blink.cmp",
+        dependencies = { "rafamadriz/friendly-snippets" },
+        version = "1.*",
+        opts = {
+            keymap = { preset = "default" },
+            appearance = {
+                nerd_font_variant = "mono",
+            },
+            completion = { documentation = { auto_show = false } },
+            sources = {
+                default = { "lsp", "path", "snippets", "buffer" },
+            },
+            fuzzy = { implementation = "lua" },
+        },
+        opts_extend = { "sources.default" },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "saghen/blink.cmp",
+            "williamboman/mason-lspconfig.nvim",
+        },
+        config = function()
+            local capabilities = require("blink.cmp").get_lsp_capabilities()
+            local lspconfig = require("lspconfig")
+            require("mason-lspconfig").setup({
+                handlers = {
+                    function(server_name)
+                        lspconfig[server_name].setup({
+                            capabilities = capabilities,
+                        })
+                    end,
+                },
+            })
+        end,
+    },
+    { "nvim-tree/nvim-web-devicons", opts = {} },
+    {
+        "ibhagwan/fzf-lua",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        cmd = "FzfLua",
+        opts = {
+            -- Use the 'ivy' layout for a clean bottom-pane feel
+            winopts = {
+                height = 0.40,
+                width = 1.00,
+                row = 1.00,
+                preview = {
+                    layout = "flex",
+                    flip_threshold = 120,
+                    scrollbar = "float",
+                },
+            },
+            keymap = {
+                -- These make the fzf window behave more like your terminal
+                fzf = {
+                    ["ctrl-q"] = "select-all+accept",
+                },
+            },
+        },
+    },
+    {
+        "akinsho/bufferline.nvim",
+        version = "*",
+        dependencies = "nvim-tree/nvim-web-devicons",
+        config = function()
+            require("bufferline").setup({})
+        end,
+    },
+    {
+        "christoomey/vim-tmux-navigator",
+        cmd = {
+            "TmuxNavigateLeft",
+            "TmuxNavigateDown",
+            "TmuxNavigateUp",
+            "TmuxNavigateRight",
+            "TmuxNavigatePrevious",
+            "TmuxNavigatorProcessList",
+        },
+        keys = {
+            { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
+            { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
+            { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
+            { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
+            { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+        },
+    },
+    {                       -- Useful plugin to show you pending keybinds.
+        "folke/which-key.nvim",
+        event = "VimEnter", -- Sets the loading event to 'VimEnter'
+        opts = {
+            -- delay between pressing a key and opening which-key (milliseconds)
+            -- this setting is independent of vim.o.timeoutlen
+            delay = 0,
+            icons = {
+                -- set icon mappings to true if you have a Nerd Font
+                mappings = vim.g.have_nerd_font,
+                -- If you are using a Nerd Font: set icons.keys to an empty table which will use the
+                -- default which-key.nvim defined Nerd Font icons, otherwise define a string table
+                keys = vim.g.have_nerd_font and {} or {
+                    Up = "<Up> ",
+                    Down = "<Down> ",
+                    Left = "<Left> ",
+                    Right = "<Right> ",
+                    C = "<C-…> ",
+                    M = "<M-…> ",
+                    D = "<D-…> ",
+                    S = "<S-…> ",
+                    CR = "<CR> ",
+                    Esc = "<Esc> ",
+                    ScrollWheelDown = "<ScrollWheelDown> ",
+                    ScrollWheelUp = "<ScrollWheelUp> ",
+                    NL = "<NL> ",
+                    BS = "<BS> ",
+                    Space = "<Space> ",
+                    Tab = "<Tab> ",
+                    F1 = "<F1>",
+                    F2 = "<F2>",
+                    F3 = "<F3>",
+                    F4 = "<F4>",
+                    F5 = "<F5>",
+                    F6 = "<F6>",
+                    F7 = "<F7>",
+                    F8 = "<F8>",
+                    F9 = "<F9>",
+                    F10 = "<F10>",
+                    F11 = "<F11>",
+                    F12 = "<F12>",
+                },
+            },
+
+            -- Document existing key chains
+            spec = {
+                { "<leader>s", group = "[S]earch" },
+                { "<leader>t", group = "[T]oggle" },
+                { "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
+            },
+        },
+    },
+    {
+        "kevinhwang91/nvim-ufo",
+        dependencies = "kevinhwang91/promise-async",
+        keys = {
+            { "<leader>fo", function() require("ufo").openAllFolds() end,  desc = "Open all folds" },
+            { "<leader>fc", function() require("ufo").closeAllFolds() end, desc = "Close all folds" },
+            { "<leader>ft", "za",                                          desc = "Toggle fold" },
+        },
+        event = "BufReadPost", -- Load when you open a file
+        init = function()
+            -- Fold settings are global, so we set them in init
+            vim.o.foldcolumn = '1' -- '0' is also fine if you don't want the side column
+            vim.o.foldlevel = 99   -- Using ufo provider need a large value
+            vim.o.foldlevelstart = 99
+            vim.o.foldenable = true
+        end,
+        config = function()
+            -- Using Treesitter as the main provider for folding functions
+            require('ufo').setup({
+                provider_selector = function(bufnr, filetype, buftype)
+                    return { 'treesitter', 'indent' }
+                end
+            })
+        end,
+    },
 })
 
 vim.cmd.colorscheme("tokyodark")
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -3,12 +3,13 @@ local keymap = vim.keymap.set
 vim.g.mapleader = " "
 
 -- Format the current buffer
-vim.keymap.set("n", "<leader><Tab>", function()
-	require("conform").format({
-		lsp_fallback = true,
-		async = false,
-		timeout_ms = 500,
-	})
+
+vim.keymap.set("n", "<leader>ff", function()
+    require("conform").format({
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 500,
+    })
 end, { desc = "Format file" })
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename symbol" })
 
@@ -23,83 +24,84 @@ vim.keymap.set("n", "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>")
 vim.keymap.set("n", "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>")
 vim.keymap.set("n", "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>")
 
-vim.keymap.set("n", "<leader>ff", "<cmd>FzfLua files<cr>", { desc = "Find Files" })
-vim.keymap.set("n", "<leader>fg", "<cmd>FzfLua live_grep<cr>", { desc = "Live Grep" })
-vim.keymap.set("n", "<leader>fb", "<cmd>FzfLua buffers<cr>", { desc = "Buffers" })
-vim.keymap.set("n", "<leader>fh", "<cmd>FzfLua help_tags<cr>", { desc = "Help Tags" })
-vim.keymap.set("n", "<leader>fr", "<cmd>FzfLua resume<cr>", { desc = "Resume Last Search" })
+vim.keymap.set("n", "<leader>sf", "<cmd>FzfLua files<cr>", { desc = "Find Files" })
+vim.keymap.set("n", "<leader>sg", "<cmd>FzfLua live_grep<cr>", { desc = "Live Grep" })
+vim.keymap.set("n", "<leader>sb", "<cmd>FzfLua buffers<cr>", { desc = "Buffers" })
+vim.keymap.set("n", "<leader>sh", "<cmd>FzfLua help_tags<cr>", { desc = "Help Tags" })
+vim.keymap.set("n", "<leader>sr", "<cmd>FzfLua resume<cr>", { desc = "Resume Last Search" })
+
 
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 for i = 1, 9 do
-	vim.keymap.set("n", "<leader>" .. i, "<Cmd>BufferLineGoToBuffer " .. i .. "<CR>", { desc = "Go to buffer " .. i })
+    vim.keymap.set("n", "<leader>" .. i, "<Cmd>BufferLineGoToBuffer " .. i .. "<CR>", { desc = "Go to buffer " .. i })
 end
 vim.keymap.set("n", "<leader>x", "<cmd>bd!<CR>", { desc = "Close current buffer" })
 
 vim.keymap.set("n", "<leader>tk", "<cmd>split | terminal<CR>", { desc = "Open terminal up" })
 local function open_term(cmd)
-	vim.cmd(cmd)
-	vim.cmd("term")
-	vim.cmd("startinsert")
+    vim.cmd(cmd)
+    vim.cmd("term")
+    vim.cmd("startinsert")
 end
 vim.keymap.set("n", "<leader>tj", function()
-	open_term("belowright split")
+    open_term("belowright split")
 end, { desc = "Open terminal down" })
 vim.keymap.set("n", "<leader>th", function()
-	open_term("vsplit")
+    open_term("vsplit")
 end, { desc = "Open terminal left" })
 vim.keymap.set("n", "<leader>tl", function()
-	open_term("belowright vsplit")
+    open_term("belowright vsplit")
 end, { desc = "Open terminal right" })
 vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
 
 vim.keymap.set("n", "<leader>ta", function()
-	vim.g.completion_enabled = not vim.g.completion_enabled
-	for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-		if vim.api.nvim_buf_is_loaded(buf) then
-			vim.b[buf].completion = vim.g.completion_enabled
-		end
-	end
-	if vim.g.completion_enabled then
-		print("Autocomplete: ON")
-	else
-		print("Autocomplete: OFF")
-	end
+    vim.g.completion_enabled = not vim.g.completion_enabled
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+        if vim.api.nvim_buf_is_loaded(buf) then
+            vim.b[buf].completion = vim.g.completion_enabled
+        end
+    end
+    if vim.g.completion_enabled then
+        print("Autocomplete: ON")
+    else
+        print("Autocomplete: OFF")
+    end
 end, { desc = "Toggle autocomplete" })
 
 local centered = false
 local group_name = "KeepCentered"
 local function enable()
-	if centered then
-		print("Centering: already ON")
-		return
-	end
-	centered = true
-	vim.api.nvim_create_augroup(group_name, { clear = true })
-	vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI", "WinEnter", "BufWinEnter", "VimResized" }, {
-		group = group_name,
-		callback = function()
-			vim.cmd("silent! normal! zz")
-		end,
-	})
-	print("Centering: ON")
+    if centered then
+        print("Centering: already ON")
+        return
+    end
+    centered = true
+    vim.api.nvim_create_augroup(group_name, { clear = true })
+    vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI", "WinEnter", "BufWinEnter", "VimResized" }, {
+        group = group_name,
+        callback = function()
+            vim.cmd("silent! normal! zz")
+        end,
+    })
+    print("Centering: ON")
 end
 
 local function disable()
-	if not centered then
-		print("Centering: already OFF")
-		return
-	end
-	centered = false
-	pcall(vim.api.nvim_del_augroup_by_name, group_name)
-	print("Centering: OFF")
+    if not centered then
+        print("Centering: already OFF")
+        return
+    end
+    centered = false
+    pcall(vim.api.nvim_del_augroup_by_name, group_name)
+    print("Centering: OFF")
 end
 
 local function toggle()
-	if centered then
-		disable()
-	else
-		enable()
-	end
+    if centered then
+        disable()
+    else
+        enable()
+    end
 end
 vim.keymap.set("n", "<leader>z", toggle, { desc = "Toggle cursor centering" })
